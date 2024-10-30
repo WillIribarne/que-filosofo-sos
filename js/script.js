4c4bb2da11d4a179fd67a9818198109c4880733f
Your Name <iriguille0504@gmail.com>
2024-10-30 12:33:17 -0300

    fade-out y fade-in, y esqueleto prototipo

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
 const contPrincipal = document.getElementById('contenidoPrincipal');
+const preguntas = [];
 
 function mostrarPortada() {
-
+    desaparecerTexto();
+    mostrarTexto('../datafiles/portada.html');
+    setTimeout(aparecerTexto, 1000);
 }
 
 function mostrarReglas() {
-
+    desaparecerTexto();
+    mostrarTexto('../datafiles/reglas.html');
+    setTimeout(aparecerTexto, 1000);
 }
 
 function mostrarInfo() {
-
+    desaparecerTexto();
+    mostrarTexto('../datafiles/masInfo.html');
+    setTimeout(aparecerTexto, 1000);
 }
 
 function empezarJuego(){
-    
+
+}
+
+function desaparecerTexto(){
+    contPrincipal.classList.remove('hola');
+    contPrincipal.classList.add('chau');
+}
+
+function aparecerTexto(){
+    contPrincipal.classList.remove('chau');
+    contPrincipal.classList.add('hola');
+}
+
+function mostrarTexto(ruta){
+    setTimeout(() =>{
+        fetch(ruta)
+        .then(response => response.text())
+        .then(data => {
+            contPrincipal.innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+    }, 1000);
 }
