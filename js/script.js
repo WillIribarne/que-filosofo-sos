1b4cdd033ee1ea7129a56f3e0e438ab5c39d96fe
Your Name <iriguille0504@gmail.com>
2024-10-30 10:54:08 -0300

    navbar y esqueletos de js creados

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const contPrincipal = document.getElementById('contenidoPrincipal');
+
+function mostrarPortada() {
+
+}
+
+function mostrarReglas() {
+
+}
+
+function mostrarInfo() {
+
+}
+
+function empezarJuego(){
+    
+}
